a9d4f85b67e7e234edc81bdef91966b0f52a4ac7
leorigaudeau <leo.rigaudeau@ynov.com>
2022-04-21 16:48:56 +0200

    add unit test for 1-Workshop

--- 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
+++ 1-intro/workshop/unit-tests/tests/TempTrackerTest.php
@@ -5,7 +5,52 @@ use PHPUnit\Framework\TestCase;
 require_once('./src/TempTracker.php');
 
 class TempTrackerTest extends TestCase {
-    public function test_something() {
-        $this->assertEqual(42, 42);
+    public function test_insert_type_error(){
+        $tracker = new TempTracker;
+        $this->expectException(\TypeError::class);
+        $tracker->insert("notAInteger");
+
     }
+
+    public function test_insert_value_error(){
+        $tracker = new TempTracker;
+        $this->expectException(\ValueError::class);
+        $this->expectException(\ValueError::class);
+
+        $tracker->insert(200);
+
+        $tracker->insert(-1);
+    }
+    public function test_temp_inserted_in_array(){
+        $tracker = new TempTracker;
+        $this->assertEquals(count($tracker->get_temps()), 0);
+        $tracker->insert(20);
+        $this->assertEquals(count($tracker->get_temps()), 1);
+    }
+
+    public function test_maximum_fct(){
+        $tracker = new TempTracker;
+        $this->assertEquals($tracker->get_max(), -1);
+        $tracker->insert(20);
+        $this->assertEquals($tracker->get_max(), 20);
+        $tracker->insert(9);
+        $this->assertEquals($tracker->get_max(), 20);
+
+    }
+    public function test_minimum_fct(){
+        $tracker = new TempTracker;
+        $this->assertEquals($tracker->get_min(), -1);
+        $tracker->insert(14);
+        $this->assertEquals($tracker->get_min(), 14);
+        $tracker->insert(12);
+        $this->assertEquals($tracker->get_min(), 12);
+    }
+    public function test_mean_fct(){
+        $tracker = new TempTracker;
+        $tracker->insert(20);
+        $this->assertEquals($tracker->get_mean(), 20);
+        $tracker->insert(0);
+        $this->assertEquals($tracker->get_mean(), 10);
+    }
+
 }
--- 1-intro/workshop/unit-tests/tests/flatten_test.php
+++ 1-intro/workshop/unit-tests/tests/flatten_test.php
@@ -6,8 +6,15 @@ require_once('./src/flatten.php');
 
 class FlattenTest extends TestCase
 {
-    public function test_something()
+    public function test_equality()
     {
-        $this->assertEqual(1337, 1337);
+        $this->assertEquals("Léo", "Léo");
+    }
+    
+    public function test_flatten_2Level(){
+        $this->assertEquals(flatten([["Léo","Henry"], ["Paul","Toma"]]), ["Léo","Henry","Paul","Toma"]);
+    }
+    public function test_flatten_3Level(){
+        $this->assertEquals(flatten([[10,[12,"R"]], ["Bureau","Annonce"]]), [10,12,"R","Bureau","Annonce"]);
     }
 }
