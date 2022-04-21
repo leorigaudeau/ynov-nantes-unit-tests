369b945efbfa33ca59eef2f66c54429ddcf731f4
Kwoak <maisonneuve.henry@gmail.com>
2022-04-21 14:32:55 +0200

    tests(e2e): add ynov website

--- 1-intro/workshop/e2e-tests/tests/some_test.js
+++ 1-intro/workshop/e2e-tests/tests/some_test.js
@@ -2,4 +2,9 @@ Feature('Ynov Nantes');
 
 Scenario('Test Ynov Nantes Land Page', ({ I }) => {
     I.amOnPage('https://www.ynov-nantes.com/');
+    I.click('a[href="/recherche/"]');
+    I.wait(3);
+    I.fillField('.searchfield','info');
+    I.click('.search-input-submit');
+    I.waitForText('Bachelor Informatique');
 });
